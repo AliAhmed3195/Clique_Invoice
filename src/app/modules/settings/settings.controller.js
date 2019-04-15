9ab0f9efb70688a4db3de3451fb974e0804cdf8d
adnanshah <adnanshah9911@gmail.com>
2019-04-15 17:24:42 +0500

    profile link added

--- src/app/modules/settings/settings.controller.js
+++ src/app/modules/settings/settings.controller.js
@@ -175,20 +175,13 @@
                 }
             }
         }
-        // getProfile()
 
         $scope.getProfileLink = function () {
             $scope.promise = SettingModel.GetProfileLink();
             $scope.promise.then(function (response) {
-                console.log("TCL: response", response)
-                // if (response.statuscode == 0) {
-                //     $scope.setting = response.data;
-                //     sessionStorage.setItem("template_color", $scope.setting.InvoiceTemplateColor);
-                //     vm.template_color = $scope.setting.InvoiceTemplateColor;
-                //     $scope.$broadcast("invoice-preview-event");
-                //     $scope.setButtonColor($scope.setting.InvoiceTemplateColor);
-                //     getInvoiceTemplates();
-                // } else {}
+                if (response.statuscode == 0) {
+                    $scope.profileLink = response.data.link
+                } else {}
             });
         }
 
