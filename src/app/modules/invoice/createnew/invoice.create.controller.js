8ff3cc7c1230d864addd944d3cb806619c9cdb3a
adnanshah <adnanshah9911@gmail.com>
2019-04-17 15:53:31 +0500

    mutli email invoices

--- src/app/modules/invoice/createnew/invoice.create.controller.js
+++ src/app/modules/invoice/createnew/invoice.create.controller.js
@@ -3,6 +3,32 @@
     angular
         .module('invoice')
         .controller('InvoiceCreateController', Controller)
+        .directive('multipleEmails', function () {
+            return {
+                require: 'ngModel',
+                link: function (scope, element, attrs, ctrl) {
+                    ctrl.$parsers.unshift(function (viewValue) {
+                        var emails = viewValue.split(',');
+                        var re = /\S+@\S+\.\S+/;
+                        var validityArr = emails.map(function (str) {
+                            return re.test(str.trim());
+                        });
+                        var atLeastOneInvalid = false;
+                        angular.forEach(validityArr, function (value) {
+                            if (value === false)
+                                atLeastOneInvalid = true;
+                        });
+                        if (!atLeastOneInvalid) {
+                            ctrl.$setValidity('multipleEmails', true);
+                            return viewValue;
+                        } else {
+                            ctrl.$setValidity('multipleEmails', false);
+                            return undefined;
+                        }
+                    });
+                }
+            }
+        })
         .filter('customInfo', function () {
 
             return function (input, optional1) {
